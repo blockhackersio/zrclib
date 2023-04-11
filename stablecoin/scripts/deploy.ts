61c8fe6f59d4f8ce865800d619313646e10c4e25
eerkaijun <kaijuneer@gmail.com>
2023-04-11 15:08:56 +0700

    define deployment order

--- stablecoin/scripts/deploy.ts
+++ stablecoin/scripts/deploy.ts
@@ -1,19 +1,37 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  // deploy ZUSD contract
   const ZUSD = await ethers.getContractFactory("ZUSD");
   const zusd = await ZUSD.deploy();
-
   await zusd.deployed();
-
   console.log(
     `ZUSD deployed to ${zusd.address}`
   );
+
+  // deploy trove manager contract
+  const TroveManager = await ethers.getContractFactory("TroveManager");
+  const troveManager = await TroveManager.deploy();
+  await troveManager.deployed();
+  console.log(
+    `TroveManager deployed to ${troveManager.address}`
+  );
+
+  // deploy borrower operations contract
+  const BorrowerOperations = await ethers.getContractFactory("BorrowOperations");
+  const borrowerOperations = await BorrowerOperations.deploy(troveManager.address);
+  await borrowerOperations.deployed();
+  console.log(
+    `BorrowerOperations deployed to ${borrowerOperations.address}`
+  );
+
+  // deploy stability pool contract
+  const StabilityPool = await ethers.getContractFactory("StabilityPool");
+  const stabilityPool = await StabilityPool.deploy(troveManager.address);
+  await stabilityPool.deployed();
+  console.log(
+    `StabilityPool deployed to ${stabilityPool.address}`
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
