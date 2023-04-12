0bcf2ecd0a48556e5ea4c69d6a040b2f2d53fd68
ryardley <contact@rudiyardley.com>
2023-04-12 14:43:15 +0700

    Setup rollup build for tools module

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-capitalize() {
-  printf '%s' "$1" | head -c 1 | tr [:lower:] [:upper:]
-  printf '%s' "$1" | tail -c '+2'
-}
-
-FNAME=$1
-FNAME_CAPS=$(capitalize $FNAME)
-
-mkdir -p ./compiled
-mkdir -p ./contracts/generated
-
-circom ./circuits/$FNAME.circom --r1cs --wasm -o ./compiled
-
-snarkjs plonk setup ./compiled/$FNAME.r1cs ./pot/pot.ptau ./compiled/$FNAME.zkey
-
-snarkjs zkey export solidityverifier ./compiled/${FNAME}.zkey ./contracts/generated/${FNAME_CAPS}Verifier.sol
-
-node ./scripts/processVerifier.js
+./scripts/compile_circuit.sh transaction
+node ./scripts/process_output.js
