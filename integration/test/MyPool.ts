77f35b0208115a38c272e44a74c566816268252a
ryardley <contact@rudiyardley.com>
2023-04-13 02:24:17 +0000

    Remove squigglys

--- integration/test/MyPool.ts
+++ integration/test/MyPool.ts
@@ -1,20 +1,23 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { plonk } from "snarkjs";
+// @ts-ignore-line
 import Utxo from "../utils/utxo";
+// @ts-ignore-line
 import { Keypair } from "../utils/keypair";
+// @ts-ignore-line
 import { prepareTransaction } from "../utils/index";
 
 it("Test transfer", async function () {
-  const sender = (await ethers.getSigners())[0]
+  const sender = (await ethers.getSigners())[0];
 
   const MyPool = await ethers.getContractFactory("MyPool");
   const pool = await MyPool.deploy();
 
   //deposit parameter
-  const depositAmount = 1e7
-  const keypair = await Keypair.create()
-  const deposit = new Utxo({ amount: depositAmount, keypair: keypair })
+  const depositAmount = 1e7;
+  const keypair = await Keypair.create();
+  const deposit = new Utxo({ amount: depositAmount, keypair: keypair });
 
   let { args, extData } = await prepareTransaction({
     outputs: [deposit],
@@ -22,22 +25,22 @@ it("Test transfer", async function () {
       owner: sender.address,
       publicKey: deposit.keypair.address(),
     },
-  })
+  });
 
   // prepare proof arguments to the correct format
-  const proof = args["proof"]
-  let pubSignals = []
-  pubSignals.push(args["root"])
-  pubSignals.push(args["publicAmount"])
-  pubSignals.push(args["extDataHash"])
+  const proof = args["proof"];
+  let pubSignals = [];
+  pubSignals.push(args["root"]);
+  pubSignals.push(args["publicAmount"]);
+  pubSignals.push(args["extDataHash"]);
   for (const element of args["inputNullifiers"]) {
-    pubSignals.push(element)
+    pubSignals.push(element);
   }
   for (const element of args["outputCommitments"]) {
-    pubSignals.push(element)
+    pubSignals.push(element);
   }
 
-  // call verify proof 
+  // call verify proof
   const mintAmount = 10;
   await pool.mint(mintAmount, proof, pubSignals);
 });
