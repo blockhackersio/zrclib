b4f883facfe7afb247c4da4d0c67506cee60a7a1
eerkaijun <kaijuneer@gmail.com>
2023-04-18 15:04:54 +0700

    remove commit details

--- stablecoin/dev-frontend/src/components/SystemStats.tsx
+++ stablecoin/dev-frontend/src/components/SystemStats.tsx
@@ -27,13 +27,6 @@ const Balances: React.FC = () => {
   );
 };
 
-const GitHubCommit: React.FC<{ children?: string }> = ({ children }) =>
-  children?.match(/[0-9a-f]{40}/) ? (
-    <Link href={`https://github.com/liquity/dev/commit/${children}`}>{children.substr(0, 7)}</Link>
-  ) : (
-    <>unknown</>
-  );
-
 type SystemStatsProps = {
   variant?: string;
   showBalances?: boolean;
@@ -113,21 +106,6 @@ export const SystemStats: React.FC<SystemStatsProps> = ({ variant = "info", show
         {total.collateralRatioIsBelowCritical(price) ? <Box color="danger">Yes</Box> : "No"}
       </Statistic>
       {}
-
-      <Box sx={{ mt: 3, opacity: 0.66 }}>
-        <Box sx={{ fontSize: 0 }}>
-          Contracts version: <GitHubCommit>{contractsVersion}</GitHubCommit>
-        </Box>
-        <Box sx={{ fontSize: 0 }}>Deployed: {deploymentDate.toLocaleString()}</Box>
-        <Box sx={{ fontSize: 0 }}>
-          Frontend version:{" "}
-          {process.env.NODE_ENV === "development" ? (
-            "development"
-          ) : (
-            <GitHubCommit>{process.env.REACT_APP_VERSION}</GitHubCommit>
-          )}
-        </Box>
-      </Box>
     </Card>
   );
 };
