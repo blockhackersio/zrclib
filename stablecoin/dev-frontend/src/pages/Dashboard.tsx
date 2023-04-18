0958ede17090d366317206f020c76f36eff692ed
eerkaijun <kaijuneer@gmail.com>
2023-04-18 14:10:20 +0700

    remove bonds

--- stablecoin/dev-frontend/src/pages/Dashboard.tsx
+++ stablecoin/dev-frontend/src/pages/Dashboard.tsx
@@ -4,16 +4,12 @@ import { Trove } from "../components/Trove/Trove";
 import { Stability } from "../components/Stability/Stability";
 import { SystemStats } from "../components/SystemStats";
 import { PriceManager } from "../components/PriceManager";
-import { Staking } from "../components/Staking/Staking";
-import { BondsTable } from "../components/Bonds/BondsTable";
 
 export const Dashboard: React.FC = () => (
   <Container variant="columns">
     <Container variant="left">
-      <BondsTable />
       <Trove />
       <Stability />
-      <Staking />
     </Container>
 
     <Container variant="right">
