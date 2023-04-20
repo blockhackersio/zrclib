ab9452b978134ab6b00662571db5e3a2e95d244a
eerkaijun <kaijuneer@gmail.com>
2023-04-20 13:31:44 +0700

    update stablecoin deployment script

--- stablecoin/hardhat.config.ts
+++ stablecoin/hardhat.config.ts
@@ -13,6 +13,12 @@ const config: HardhatUserConfig = {
       },
     }
   },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
   mocha: {
     timeout: 100_000_000, // sets the timeout to 10 seconds
   }
