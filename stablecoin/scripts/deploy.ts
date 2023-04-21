4cb269b14aed37b6ff708ddeabb5c8ec8626b537
eerkaijun <kaijuneer@gmail.com>
2023-04-21 09:01:14 +0700

    update deployment script

--- stablecoin/scripts/deploy.ts
+++ stablecoin/scripts/deploy.ts
@@ -1,29 +1,43 @@
 import { ethers } from "hardhat";
+import path from "path";
+
+const artifactPath = path.join(
+  __dirname,
+  "../../tools/contracts/generated/Hasher.json"
+);
+const artifact = require(artifactPath);
 
 async function main() {
-  // deploy ZUSD contract
-  const ZUSD = await ethers.getContractFactory("ZUSD");
-  const zusd = await ZUSD.deploy();
-  await zusd.deployed();
-  console.log(
-    `ZUSD deployed to ${zusd.address}`
-  );
-
-  // deploy trove manager contract
+  // deploy trove manager
   const TroveManager = await ethers.getContractFactory("TroveManager");
   const troveManager = await TroveManager.deploy();
   await troveManager.deployed();
-  console.log(
-    `TroveManager deployed to ${troveManager.address}`
-  );
+  console.log("TroveManager deployed to:", troveManager.address);
 
-  // deploy stability pool contract
+  // deploy stability pool
   const StabilityPool = await ethers.getContractFactory("StabilityPool");
   const stabilityPool = await StabilityPool.deploy(troveManager.address);
   await stabilityPool.deployed();
-  console.log(
-    `StabilityPool deployed to ${stabilityPool.address}`
+  console.log("StabilityPool deployed to:", stabilityPool.address);
+
+  // deploy Poseidon hasher contract
+  const Hasher = await ethers.getContractFactory(
+    artifact.abi,
+    artifact.bytecode
   );
+  const hasher = await Hasher.deploy();
+  await hasher.deployed();
+  console.log("Hasher deployed to:", hasher.address);
+
+  // deploy ZUSD
+  const ZUSD = await ethers.getContractFactory("ZUSD");
+  const zusd = await ZUSD.deploy(hasher.address, troveManager.address, stabilityPool.address);
+  await zusd.deployed();
+  console.log("ZUSD deployed to:", zusd.address);
+
+  // set addresses
+  const chainlinkAddress = "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e"; // ETH/USD price feed on Goerli
+  await troveManager.setAddresses(zusd.address, stabilityPool.address, chainlinkAddress);
 }
 
 main()
