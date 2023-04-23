812f1d082186ad6cfb47a214b4c5ed4761a189c9
eerkaijun <kaijuneer@gmail.com>
2023-04-23 09:17:58 +0700

    add multi asset circuit

--- sdk/scripts/build.sh
+++ sdk/scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./scripts/compile_circuit.sh transaction
+./scripts/compile_circuit.sh multitransaction
 node ./scripts/process_output.js
 node ./scripts/generate_hasher_contract.js
 pnpm tsc
