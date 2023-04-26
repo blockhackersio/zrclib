37c2c0f7d94a151ce6c4bc40f6e3d4c81479d1c3
ryardley <contact@rudiyardley.com>
2023-04-26 09:30:45 +0000

    Update padding

--- examples/wallet/components/BaseStack.tsx
+++ examples/wallet/components/BaseStack.tsx
@@ -6,9 +6,9 @@ export function BaseStack() {
   const { address, isConnected } = useAccount();
 
   return (
-    <div className="text-center w-full p-4">
+    <div className="text-center w-full min-h-screen p-4">
       {!isConnected ? (
-        <Vertical>
+        <Vertical className="h-full p-20">
           <div className="text-lg mb-4">Connect your Wallet!</div>
           <div className="text-md">
             You need to be connected to use the coinshield wallet
--- examples/wallet/ui/Vertical.tsx
+++ examples/wallet/ui/Vertical.tsx
@@ -6,13 +6,14 @@ export function Vertical({
   center = false,
   ...p
 }: {
+  className: string;
   children: ReactNode;
   gap?: boolean;
   center?: boolean;
 }) {
   return (
     <div
-      className={classNames("flex flex-col justify-items-center", {
+      className={classNames("flex flex-col justify-items-center", p.className, {
         "gap-4": gap,
         "justify-center": center,
       })}
