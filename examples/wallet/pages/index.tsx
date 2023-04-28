5763862efd395a8bf6e4fce82b4f1bd752ddc129
ryardley <contact@rudiyardley.com>
2023-04-28 05:15:40 +0000

    remove redundant code

--- examples/wallet/pages/index.tsx
+++ examples/wallet/pages/index.tsx
@@ -1,9 +1,6 @@
 import { PageLayout } from "@/components/Layout";
 import { HomePanel } from "@/components/HomePanel";
-import { getWasmFileLocation, getZkeyFileLocation } from "@zrclib/sdk";
 export default function Home() {
-  console.log(getWasmFileLocation());
-  console.log(getZkeyFileLocation());
   return (
     <PageLayout title="coinshield">
       <HomePanel />
