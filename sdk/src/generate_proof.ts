d612b018157636143415359f0f53270422d0cdbe
ryardley <contact@rudiyardley.com>
2023-05-07 20:01:01 +0000

    Update proof preparation for g16

--- sdk/src/generate_proof.ts
+++ sdk/src/generate_proof.ts
@@ -1,5 +1,6 @@
 import { plonk, groth16 } from "snarkjs";
 import { getWasmFileLocation, getZkeyFileLocation } from "./key_locator";
+import { toFixedHex } from "./utils";
 
 export async function generatePlonkProof(inputs: object) {
   const wasmLocation = getWasmFileLocation();
@@ -8,9 +9,10 @@ export async function generatePlonkProof(inputs: object) {
   console.log(JSON.stringify(inputs));
   console.log("/generatePlonkProof=========");
   const { proof } = await plonk.fullProve(inputs, wasmLocation, zkeyLocation);
-  const calldata = await plonk.exportSolidityCallData(proof, []);
+  const calldata: string = await plonk.exportSolidityCallData(proof, []);
+  // Calldata comes with the inputs added at the end
   const [proofString] = calldata.split(",");
-  return proofString as string;
+  return proofString;
 }
 
 export async function generateGroth16Proof(inputs: object) {
@@ -20,9 +22,17 @@ export async function generateGroth16Proof(inputs: object) {
   console.log(JSON.stringify(inputs));
   console.log("/generateGroth16Proof=========");
   const { proof } = await groth16.fullProve(inputs, wasmLocation, zkeyLocation);
-  const calldata = await groth16.exportSolidityCallData(proof, []);
-  const [proofString] = calldata.split(",");
-  return proofString as string;
+  return (
+    "0x" +
+    toFixedHex(proof.pi_a[0]).slice(2) +
+    toFixedHex(proof.pi_a[1]).slice(2) +
+    toFixedHex(proof.pi_b[0][1]).slice(2) +
+    toFixedHex(proof.pi_b[0][0]).slice(2) +
+    toFixedHex(proof.pi_b[1][1]).slice(2) +
+    toFixedHex(proof.pi_b[1][0]).slice(2) +
+    toFixedHex(proof.pi_c[0]).slice(2) +
+    toFixedHex(proof.pi_c[1]).slice(2)
+  );
 }
 
 export type GenerateProofFn = (inputs: object) => Promise<string>;
