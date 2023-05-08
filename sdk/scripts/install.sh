50b4daa8c7da4f8d59d350dafd1d855635f50781
ryardley <contact@rudiyardley.com>
2023-05-08 20:17:13 +0000

    Update scripts

--- sdk/scripts/install.sh
+++ sdk/scripts/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -n "$SKIP_CIRCOM" ]; then
+  exit 0
+fi
+
 mkdir -p pot
 
 if [ ! -f ./pot/pot.ptau ]; then
@@ -8,7 +12,7 @@ fi
 
 B2_DOWNLOAD=$(b2sum pot/pot.ptau | awk '{ print $1 }')
 
-if [ "$B2_DOWNLOAD" = "6247a3433948b35fbfae414fa5a9355bfb45f56efa7ab4929e669264a0258976741dfbe3288bfb49828e5df02c2e633df38d2245e30162ae7e3bcca5b8b49345" ];then 
+if [ "$B2_DOWNLOAD" = "6247a3433948b35fbfae414fa5a9355bfb45f56efa7ab4929e669264a0258976741dfbe3288bfb49828e5df02c2e633df38d2245e30162ae7e3bcca5b8b49345" ]; then
   echo "Ptau file has been validated and is correct!"
 else
   echo "Danger!!! Powers of Tau file is incorrect. "
