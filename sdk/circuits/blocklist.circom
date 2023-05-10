09534114e957b54b6c17e0a231335790609e430d
eerkaijun <kaijuneer@gmail.com>
2023-05-10 14:27:43 +0100

    verify zk in blocklist contract

--- sdk/circuits/blocklist.circom
+++ sdk/circuits/blocklist.circom
@@ -26,7 +26,7 @@ template UpdateBlocklist(levels) {
     signal input pathIndices;
     signal input pathElements[levels];
     signal input oldRoot;
-    signal output newRoot;
+    signal input newRoot;
 
     component checkInitialPath = MerkleProof(levels);
     checkInitialPath.leaf <== 19014214495641488759237505126948346942972912379615652741039992445865937985820; // poseidon(0) which is allowed leaf
@@ -38,7 +38,7 @@ template UpdateBlocklist(levels) {
     checkUpdatedPath.leaf <== 18586133768512220936620570745912940619677854269274689475585506675881198879027; // poseidon(1) which is blocked leaf
     checkUpdatedPath.pathIndices <== pathIndices;
     checkUpdatedPath.pathElements <== pathElements;
-    newRoot <== checkUpdatedPath.root;
+    newRoot === checkUpdatedPath.root;
 }
 
-component main { public [oldRoot] } = UpdateBlocklist(5);
+component main { public [oldRoot, newRoot] } = UpdateBlocklist(5);
--- sdk/contracts/MerkleTreeForBlocklist.sol
+++ sdk/contracts/MerkleTreeForBlocklist.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.0;
 
+import {BlocklistVerifier} from "./generated/BlocklistVerifier.sol";
+
 contract MerkleTreeForBlocklist {
 
     /// @dev all leaves are initialized as poseidon(0), which means allowed leaves
@@ -11,20 +13,53 @@ contract MerkleTreeForBlocklist {
     bytes32 root; // root of the sparse Merkle Tree
     mapping(uint256 => bool) blockIndices;
 
+    struct ProofArguments {
+        bytes proof;
+        bytes32 oldRoot;
+        bytes32 newRoot;
+    }
+
+    BlocklistVerifier public verifier;
+
     event newBlockDeposit(uint256 index);
 
-    constructor(uint32 _levels) {
+    constructor(uint32 _levels, address _verifier) {
         require(_levels > 0, "_levels should be greater than zero");
         require(_levels < 32, "_levels should be less than 32");
         levels = _levels;
         // initialize sparse Merkle Tree where all leaves are poseidon(0)
         root = 0x2b94cf5e8746b3f5c9631f4c5df32907a699c58c94b2ad4d7b5cec1639183f55;
+        verifier = BlocklistVerifier(_verifier);
     }
 
-    function blockDeposit(bytes32 newRoot, uint256 index) public {
+    function parseProof(
+        bytes memory data
+    ) public pure returns (uint[2] memory a, uint[2][2] memory b, uint[2] memory c) {
+        (a[0], a[1], b[0][0], b[0][1], b[1][0], b[1][1], c[0], c[1]) = abi
+            .decode(data, (uint, uint, uint, uint, uint, uint, uint, uint));
+    }
+
+    function verifyGroth16Proof(
+        bytes memory _proof,
+        uint[2] memory _pubSignals
+    ) internal view returns (bool) {
+        (uint[2] memory a, uint[2][2] memory b, uint[2] memory c) = parseProof(
+            _proof
+        );
+        return verifier.verifyProof(a, b, c, _pubSignals);
+    }
+
+    function blockDeposit(ProofArguments calldata proof, uint256 index) public {
         // TODO: set modifier to allow only governance can block a deposit
-        // TODO: generate a zero knowledge proof that shows that root is updated correctly
-        root = newRoot;
+        // verify zero knowledge proof that shows that root is updated correctly
+        uint[2] memory pubSignals = [
+            uint(proof.oldRoot),
+            uint(proof.newRoot)
+        ];
+        verifyGroth16Proof(proof.proof, pubSignals);
+
+        // update root and indices
+        root = proof.newRoot;
         blockIndices[index] = true;
         emit newBlockDeposit(index);
     } 
