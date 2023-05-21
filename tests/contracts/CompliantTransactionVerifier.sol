6517954d0866c1175c669ad0c5a6036d5a5170c1
eerkaijun <kaijuneer@gmail.com>
2023-05-21 10:40:36 +0100

    add circuit verifier

---
+++ tests/contracts/CompliantTransactionVerifier.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {CompliantVerifier} from "@zrclib/sdk/contracts/generated/CompliantVerifier.sol";
+
+contract CompliantTransactionVerifier is CompliantVerifier {}
