d9d1a1ebc6470599032613c1a95c78359bf51212
eerkaijun <kaijuneer@gmail.com>
2023-05-21 11:38:20 +0100

    test deposit into compliant pool - wip

--- tests/test/compliance.test.ts
+++ tests/test/compliance.test.ts
@@ -7,7 +7,20 @@ import {
     MockErc20__factory,
     SwapExecutor__factory,
 } from "../typechain-types";
+import { Account } from "@zrclib/sdk";
 import artifact from "@zrclib/sdk/contracts/generated/Hasher.json";
+import { tend, time } from "../utils";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+
+async function deployERC20Token(name: string, symbol: string) {
+    // Prepare signers
+    const [deployer] = await ethers.getSigners();
+  
+    const erc20Factory = new MockErc20__factory(deployer);
+    const token = await erc20Factory.deploy(name, symbol);
+  
+    return token;
+}
 
 async function setup() {
     // Prepare signers
@@ -36,22 +49,45 @@ async function setup() {
         blocklistVerifier.address,
         swapExecutor.address
     );
-  
-    return { contract };
-}
 
-async function deployERC20Token(name: string, symbol: string) {
-    // Prepare signers
-    const [deployer] = await ethers.getSigners();
-  
-    const erc20Factory = new MockErc20__factory(deployer);
-    const token = await erc20Factory.deploy(name, symbol);
-  
-    return token;
+    let token = await deployERC20Token("TEST", "TEST");
+
+    return { contract, token };
 }
   
 it("Test deposit", async function() {
+    const TEN = 10 * 1_000_000;
 
+    const [deployer, aliceEth] = await ethers.getSigners();
+
+    let { contract, token } = await loadFixture(setup);
+
+    // CREATE ACCOUNTS
+    const alice = await Account.create(contract, aliceEth, "password123");
+    await alice.login();
+
+    let tx, t, proof, publicBalance, privateBalance;
+
+    // MINT TOKENS
+    token = token.connect(deployer);
+    tx = await token.mint(aliceEth.address, TEN);
+    await tx.wait();
+
+    contract = contract.connect(aliceEth);
+    token = token.connect(aliceEth);
+
+    /// DEPOSIT
+    // t = time("Alice creates shield proof for 10 coins");
+    // proof = await alice.proveShield(TEN, token.address);
+    // tend(t);
+
+    // t = time("Alice approves ERC20 payment");
+    // await token.approve(contract.address, TEN);
+    // tend(t);
+
+    // t = time("Alice submits transaction");
+    // await contract.transact(proof);
+    // tend(t);
 });
 
 it("Test add leaf to blocklist", async function() {
