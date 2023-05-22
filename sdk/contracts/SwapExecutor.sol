6b5070f2db11191f491bc14dac7b111fc3697877
eerkaijun <kaijuneer@gmail.com>
2023-05-22 18:47:52 +0100

    remove logging

--- sdk/contracts/SwapExecutor.sol
+++ sdk/contracts/SwapExecutor.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./ShieldedPool.sol";
-import "hardhat/console.sol";
 
 // This contract is used to execute external swap, leading to an arbitrary external call.
 // It should never hold any funds, get any token allowance, or manage any contracts.
@@ -29,8 +28,6 @@ contract SwapExecutor {
             require(success, "SwapExecutor: swap failed");
         }
 
-        console.log("Breakpoint1");
-
         // The transfer can also be done in the swap router directly, but we do it here because we want to enable re-shield feature later.
         uint256 amountOut = IERC20(_tokenOut).balanceOf(address(this));
         require(
@@ -41,7 +38,6 @@ contract SwapExecutor {
         // if _swapRecipient is zero, we should re-shield the token.
         // if _swapRecipient is not zero, we should transfer the token to _swapRecipient.
         if (_swapRecipient == address(0)) {
-            console.log("Breakpoint2");
             // re-shield token
             // user should generate proof in advance
             (
@@ -51,18 +47,14 @@ contract SwapExecutor {
                     _transactData,
                     (ShieldedPool.ProofArguments, ShieldedPool.ExtData)
                 );
-            console.log("Breakpoint3");
             IERC20(_tokenOut).safeApprove(msg.sender, 0);
             IERC20(_tokenOut).safeApprove(msg.sender, _amountOutMin);
-            console.log("Breakpoint4");
             ShieldedPool(msg.sender).transact(
                 ShieldedPool.Proof(_args, _extData)
             );
-            console.log("Breakpoint5");
             // TODO: there would be some changes in the swap, we transfer it to tx.origin to reward relayer.
         } else {
             // transfer token to _swapRecipient
-            console.log("Breakpoint6");
             IERC20(_tokenOut).safeTransfer(_swapRecipient, amountOut);
         }
     }
